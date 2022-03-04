a1132cc8f14b09e6c5dc9e5b723e1a6fec29f7e5
Alexandr Korsak <alex.korsak@gmail.com>
2022-03-04 22:27:12 +0100

    fix header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
                     href='#items-section'
                     className='inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
                   >
-                    Lista de la compra
+                    Lista de compras
                   </a>
                   <a
                     href='#addresses-section'
@@ -59,7 +59,7 @@ export default function Header() {
                 href='#items-section'
                 className='block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700'
               >
-                Lista de la compra
+                Lista de compras
               </Disclosure.Button>
               <Disclosure.Button
                 as='a'
